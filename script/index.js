4abd3d2d98714a3c5f9816ff81e5ff84997ae7ff
Muhammad Kassar <36678834+mk360@users.noreply.github.com>
2024-06-28 05:43:23 +0200

    improve character bar, organize character thumbnails

--- script/index.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("ui").onchange = function(e) {
-    const { checked } = e.target;
-    if (checked) {
-        document.body.classList.add("enable-ui");
-    } else {
-        document.body.classList.remove("enable-ui");
-    }
-}
