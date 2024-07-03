b582d1d2d04ba7065d9bb0c5f4a006deb39096e2
Muhammad Kassar <36678834+mk360@users.noreply.github.com>
2024-07-03 23:50:03 +0200

    start page interactions

--- script/index.js
+++ script/index.js
@@ -1,5 +1,56 @@
-const characterButtons = document.querySelectorAll(".character-grid button");
+(function() {
+    const characterSelect = document.getElementById("character-select-list");
 
-for (let i = 0; i < characterButtons.length; i++) {
-    const button = characterButtons.item(i);
-}
+    let p1 = "ryu";
+    let p2 = "ken";
+    let chosenSide = "p1";
+
+    const characterButtons = document.querySelectorAll(".character-grid button");
+
+    const p1CharacterSelect = document.getElementById("p1");
+    const p2CharacterSelect = document.getElementById("p2");
+
+    p1CharacterSelect.onclick = function() {
+        chosenSide = "p1";
+        toggleClass(characterSelect, "show");
+        characterSelect.classList.remove("p2-select");
+    };
+
+    p2CharacterSelect.onclick = function() {
+        toggleClass(characterSelect, "show");
+        chosenSide = "p2";
+        characterSelect.classList.add("p2-select");
+    };
+
+    for (let i = 0; i < characterButtons.length; i++) {
+        const button = characterButtons.item(i);
+        button.onclick = function() {
+            const characterName = this.id;
+            updateCurrentCharacter(chosenSide, characterName);
+            toggleClass(characterSelect, "show");
+        }
+    }
+
+    function loadVideos() {
+
+    }
+
+    function updateCurrentCharacter(side, character) {
+        const targetElement = side === "p1" ? p1CharacterSelect : p2CharacterSelect;
+        const nameElement = targetElement.getElementsByClassName("name")[0];
+        nameElement.textContent = character;
+
+        const characterImage = targetElement.getElementsByClassName("character-image")[0];
+        const oldCharacterClass = Array.prototype.find.call(characterImage.classList, (l) => l.includes("-thumbnail"));
+        characterImage.classList.remove(oldCharacterClass);
+        characterImage.classList.add(`${character}-thumbnail`);
+    }
+
+    function toggleClass(element, cssClass) {
+        if (Array.prototype.includes.call(element.classList, cssClass)) {
+            element.classList.remove(cssClass);
+        } else {
+            element.classList.add(cssClass);
+        }
+    }
+})();
